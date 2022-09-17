6f9d952bfd3bbbf82cf36d00a57cdd3c3e7b6dc0
GentlyTech <andyyep@hotmail.com>
2022-09-16 23:19:22 -0400

    CreateGroupPage skeleton

---
+++ src/Views/FriendView/Pages/CreateGroupPage/CreateGroupPage.tsx
@@ -0,0 +1,25 @@
+import { useTheme } from "@mui/material";
+import useClassNames from "Hooks/useClassNames";
+import { useTranslation } from "react-i18next";
+
+import PageContainer from "Components/Containers/PageContainer/PageContainer";
+
+import type { Page } from "DataTypes/Components";
+
+interface CreateGroupPageProps extends Page {
+
+}
+
+function CreateGroupPage(props: CreateGroupPageProps) {
+  const Localizations_CreateGroupPage = useTranslation("CreateGroupPage").t;
+  const classNames = useClassNames("CreateGroupPageContainer", props.className);
+  const theme = useTheme();
+
+  return (
+    <PageContainer className={classNames} noPadding>
+
+    </PageContainer>
+  );
+}
+
+export default Page;
