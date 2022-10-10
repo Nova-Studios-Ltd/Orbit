6f54e98fb68dba529029e735827f6ab93f68049e
Aiden Wilkins <aidencw01@gmail.com>
2022-10-10 18:35:56 -0400

    Updated API domain

--- src/vars.ts
+++ src/vars.ts
@@ -4,5 +4,5 @@ export const APP_VERSION = manifest.version;
 export const DEBUG = false;
 
 // Control values for API
-export const API_DOMAIN = "https://api.novastudios.uk"; /* http://, https:// */
-export const WEBSOCKET_DOMAIN = "wss://api.novastudios.uk"; /* ws:// wss:// */
+export const API_DOMAIN = "https://live.api.novastudios.uk"; /* http://, https:// */
+export const WEBSOCKET_DOMAIN = "wss://live.api.novastudios.uk"; /* ws:// wss:// */
