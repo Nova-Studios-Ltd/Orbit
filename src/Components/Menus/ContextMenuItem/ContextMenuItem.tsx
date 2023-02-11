0dfbd79af8db06ee6109fb09896a1b3fdc150e6c
GentlyTech <48165845+GentlyTech@users.noreply.github.com>
2023-02-10 20:20:52 -0500

    Styling stuff

--- src/Components/Menus/ContextMenuItem/ContextMenuItem.tsx
+++ src/Components/Menus/ContextMenuItem/ContextMenuItem.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode, useState } from "react";
 import { MenuItem, useTheme } from "@mui/material";
 import useClassNames from "Hooks/useClassNames";
+import useStyles from "Hooks/useStyles";
 
 import type { NCComponent } from "Types/UI/Components";
 import type { ReactSVGElement } from "react";
@@ -19,6 +20,8 @@ function ContextMenuItem(props: ContextMenuItemProps) {
   const classNames = useClassNames("ContextMenuItemContainer", props.className);
 
   const [backgroundColor, setBackgroundColor] = useState(theme.customPalette.contextMenuItemBackground);
+
+  const styles = useStyles({ color: theme.palette.text.primary, backgroundColor: backgroundColor }, props.style)
 
   if (props.hide) return null;
 
@@ -39,7 +42,7 @@ function ContextMenuItem(props: ContextMenuItemProps) {
   }
 
   return (
-    <MenuItem className={classNames} style={{ color: theme.palette.text.primary, backgroundColor: backgroundColor }} disabled={props.disabled} onClick={handleLeftClick} onContextMenu={handleRightClick} onMouseEnter={() => handleFocus(true)} onFocus={() => handleFocus(true)} onMouseLeave={() => handleFocus(false)} onBlur={() => handleFocus(false)}>
+    <MenuItem className={classNames} style={styles} disabled={props.disabled} onClick={handleLeftClick} onContextMenu={handleRightClick} onMouseEnter={() => handleFocus(true)} onFocus={() => handleFocus(true)} onMouseLeave={() => handleFocus(false)} onBlur={() => handleFocus(false)}>
       {props.icon}
       {props.children}
     </MenuItem>
