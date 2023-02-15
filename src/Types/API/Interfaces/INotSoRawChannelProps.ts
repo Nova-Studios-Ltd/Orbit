b9dbf840d493a2f6fe2c052832918d3b25e093b5
GentlyTech <48165845+GentlyTech@users.noreply.github.com>
2023-02-15 13:59:53 -0500

    Make ui.members optional, cleanup unused imports

--- src/Types/API/Interfaces/INotSoRawChannelProps.ts
+++ src/Types/API/Interfaces/INotSoRawChannelProps.ts
@@ -1,9 +1,8 @@
 import { IRawChannelProps } from "Types/API/Interfaces/IRawChannelProps";
-import type { ChannelTypes } from "Types/Enums";
 import type Friend from "Types/UI/Friend";
 
 export interface INotSoRawChannelProps extends IRawChannelProps {
   ui?: {
-    members: Friend[]
+    members?: Friend[],
   }
 }
